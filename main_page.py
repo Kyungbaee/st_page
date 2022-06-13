cd74f3c1b0ce95c807f7265ee21a77bc89497c8a
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2022-06-13 14:38:30 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -8,6 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+## st.cache 매번 호출할 필요없이 캐시된 데이터 사용
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
